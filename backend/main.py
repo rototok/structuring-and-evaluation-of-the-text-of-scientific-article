40886116c91c34a289e426111ccc27f1d69f76e1
rototok <alexandro-tolstenko@mail.ru>
2026-03-03 18:30:00 +0300

    completed frontend, mock fastAPI, integration of frontend and fastAPI

---
+++ backend/main.py
@@ -0,0 +1,19 @@
+from .api import analyzers
+
+from fastapi import FastAPI
+
+
+app = FastAPI(
+    title="Academic Writing Assistant API",
+    description="Backend API for LLM-based academic writing analysis",
+    version="0.1.0"
+)
+
+app.include_router(analyzers.router)
+
+@app.get("/", tags=["Health"])
+def healthcheck():
+    return {
+        "status": "Backend is running",
+        "service": "Academic Writing Assistant API"
+    }
--- frontend/app.py
+++ frontend/app.py
@@ -1,43 +1,100 @@
 from io import BytesIO
+import os
 import requests
 import streamlit as st
+import time
 
 
-def structure_analyze(uploaded_file):
+# url подтягивается из docker-compose.yml
+BACKEND_URL = os.getenv("BACKEND_URL", "http://localhost:8000")
+
+
+# так как streamlit перезапускает скрипт после каждого действия пользователя,
+# отрисовать все в колбэке по нажатию кнопку не представляется возможным
+# именно для того, чтобы всё корректно и вовремя отражалось, нужны стейты сессии
+if "task_id" not in st.session_state: # id таски в celery
+    st.session_state.task_id = None
+
+if "status" not in st.session_state: # статус таски в celery
+    st.session_state.status = None
+
+if "running" not in st.session_state: # флаг, отражающий необходимость ожидания ответа от celery
+    st.session_state.running = False
+
+if "result" not in st.session_state: # ответ от бэка для отображения на странице
+    st.session_state.result = None
+
+
+# кол-бэк для кнопок, который отправляет запрос по API
+def analyze_file(uploaded_file, module):
     bytes_io = BytesIO(uploaded_file.getvalue())
-    # TODO: Нужно вызвать API бэка
-    answer = requests() 
-    write_answer(answer)    
+
+    # так как мы уже считали файл, подаем его в формате (название, данные в байтах, тип файла (application/type))
+    files = {"file": (uploaded_file.name, bytes_io, uploaded_file.type)}
+
+    response = requests.post(
+         f"{BACKEND_URL}/analyzers/{module}",
+         files=files,
+         timeout=30
+    ) 
+
+    if response.status_code != 202:
+         st.error(f"Error: {response.text}")
+         return
+    
+    data = response.json()
+    
+    st.session_state.task_id = data["task_id"]
+    st.session_state.status = "PENDING"
+    st.session_state.running = True
+
+    st.info(f"Task submitted. Task_id: {st.session_state.task_id}")
 
 
-def style_analyze(uploaded_file):
-    bytes_io = BytesIO(uploaded_file.getvalue())
-    # TODO: Нужно вызвать API бэка
-    answer = requests() 
-    write_answer(answer)    
+# функция для проверки статуса задачи
+# как только получили ответ от бэка - меняем статус
+def check_status():
+    status_response = requests.get(
+         f"{BACKEND_URL}/status/{st.session_state.task_id}",
+         timeout=10
+         )
+       
+    status_data = status_response.json()
+    status = status_data["status"]
+       
+    if status == "SUCCESS":
+       result_response = requests.get(
+            f"{BACKEND_URL}/result/{st.session_state.task_id}",
+            timeout=30
+            )
+       st.session_state.result = result_response.json()["result"]
+       st.session_state.status = status
+       st.session_state.running = False
 
-
-def logic_analyze(uploaded_file):
-    bytes_io = BytesIO(uploaded_file.getvalue())
-    # TODO: Нужно вызвать API бэка
-    answer = requests() 
-    write_answer(answer)
-
-
-def write_answer(answer):
-   flex.write("### Answer")
-   flex.write(answer)
+    elif status == "FAILURE":
+       st.error("Task failed")
 
 
 st.title("AI Academic Writing Assistant")
 uploaded_file = st.file_uploader(label="Upload your manuscript", type=["txt", "docx", "pdf"])
+
 flex = st.container()
 if uploaded_file is not None:
-    flex_1 = flex.container(horizontal=True, horizontal_alignment="center")
+    container = flex.container(horizontal=True, horizontal_alignment="center")
 
-    flex_1.button("Structure", on_click=structure_analyze, args=[uploaded_file])
-    flex_1.button("Style", on_click=style_analyze, args=[uploaded_file])
-    flex_1.button("Readability and Logic", on_click=logic_analyze, args=[uploaded_file])
+    container.button("Structure", on_click=analyze_file, args=[uploaded_file, "structure"])
+    container.button("Style", on_click=analyze_file, args=[uploaded_file, "style"])
+    container.button("Readability and Logic", on_click=analyze_file, args=[uploaded_file, "logic"])
 
 flex.divider()
 
+
+if st.session_state.running:
+    check_status()
+    st.info(st.session_state.status)
+    time.sleep(2)
+    st.rerun()
+
+if st.session_state.result:
+    flex.write("### Answer")
+    flex.write(st.session_state.result)
